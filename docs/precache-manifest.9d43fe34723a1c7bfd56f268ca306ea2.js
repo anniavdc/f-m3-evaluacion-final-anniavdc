1f2d1b909ae7b690e7490017532ccc4d7f27c727
anniavdc <anva1189@gmail.com>
2019-05-27 15:24:12 +0200

    Add docs folder

---
+++ docs/precache-manifest.9d43fe34723a1c7bfd56f268ca306ea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d8d2e7d55a3febda73484f98db976f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d05b494fe3590464108b",
+    "url": "./static/css/main.1432d3d7.chunk.css"
+  },
+  {
+    "revision": "2c47f0cafbd0b9f00a6c",
+    "url": "./static/js/2.7abb71b8.chunk.js"
+  },
+  {
+    "revision": "d05b494fe3590464108b",
+    "url": "./static/js/main.106d5afa.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "493650fb0da320e054f355d0b96f84ec",
+    "url": "./static/media/howarts.493650fb.png"
+  }
+]);
